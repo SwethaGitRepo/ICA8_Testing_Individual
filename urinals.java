5f3162b6b6cf0294d7eecda955d2f90dde82a55b
Swetha Ganapathy (Student) <sganap12@sundevils.asu.edu>
2022-10-27 15:35:49 -0700

    Test failure case

--- urinals.java
+++ urinals.java
@@ -1,8 +1,28 @@
 //Author - Swetha Ganapathy
+import java.util.Scanner;
+
 public class urinals {
-    public static void main(String[] args)
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        if(goodString(String.valueOf(sc))) {
+            System.out.println("Valid String");
+        }
+    }
+
+    public static Boolean goodString(String str)
     {
-        
+        // checks to see if valid string
+        int len = str.length();
+        for(int i=0; i<len; i++) {
+            if (str.charAt(i) != '0' && str.charAt(i) != '1') {
+                return false;
+            }
+            if(i < len-1 && str.charAt(i) == str.charAt(i+1) && str.charAt(i) == '1')
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
---
+++ urinalsTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void goodString() {
+        assertEquals(true, urinals.goodString("abcdef"));
+        System.out.println("====== Swetha Ganapathy == TEST ONE EXECUTED =======");
+    }
+}
