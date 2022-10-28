bd106bd1c82b83a7b8a50b459f3998fe186d6415
Swetha Ganapathy (Student) <sganap12@sundevils.asu.edu>
2022-10-27 22:32:10 -0700

    corrections after review

--- urinals.java
+++ urinals.java
@@ -6,82 +6,93 @@ public class urinals {
     public static void main(String[] args) throws Exception {
         //get input choice from user - 1.keyboard input 2.File input
         String choice = getChoice();
-        validateChoice(choice);
-        if(choice.equals("1"))
-        {
-            System.out.println("Enter the string");
-            Scanner strSc = new Scanner(System.in);
-            String str = strSc.nextLine();
-            int count = findMaxNumOfFreeUrinals(String.valueOf(str));
-            System.out.println("Number of free urinals : "+count);
-        }
-        else if(choice.equals("2"))
-        {
-            readFileAndFindMaxNumOfFreeUrinals();
+        if (validateChoice(choice)) {
+            if (choice.equals("1")) {
+                System.out.println("Enter the string");
+                Scanner strSc = new Scanner(System.in);
+                String str = strSc.nextLine();
+                int count = findMaxNumOfFreeUrinals(String.valueOf(str));
+                System.out.println("Number of free urinals :");
+                System.out.println(count);
+            } else if (choice.equals("2")) {
+                readFileAndFindMaxNumOfFreeUrinals();
+            }
         }
     }
 
-    public static String getChoice()
-    {
+    public static String getChoice() {
         System.out.println("Enter your choice : ");
         System.out.println("1.Keyboard Input 2.File Input");
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
 
-    public static Boolean validateChoice(String choice)
-    {
-        if(choice != "1" && choice != "2")
-        {
-            return false;
-        }
-        return true;
+    public static Boolean validateChoice(String choice) {
+        return choice.equals("1") || choice.equals("2");
     }
+
     public static void readFileAndFindMaxNumOfFreeUrinals() throws Exception {
-        BufferedReader inputFile;
-        inputFile = new BufferedReader(new FileReader("urinal.dat"));
+        String fileName = "urinal.dat";
+        BufferedReader inputFile = readFile(fileName);
         String str;
         String outputFile = getOutputFile();
-        while ((str = inputFile.readLine()) != null && !str.equals("-1"))
-        {
+        while ((str = inputFile.readLine()) != null && !str.equals("-1")) {
             int count = findMaxNumOfFreeUrinals(str);
             writeIntoFile(outputFile, count);
         }
     }
 
-    public static String getOutputFile() throws Exception{
-            File counterFile = new File("counter.txt");
-            Scanner ctr = new Scanner(counterFile);
-            int counter = Integer.parseInt(ctr.nextLine());
-            String outputFile = "rule.txt";
-            if (counter != 0) {
-                outputFile = "rule" + counter + ".txt";
-            }
-            incrementCounter(counter);
-            System.out.println("Successfully written output to " + outputFile);
-            return outputFile;
-    }
-    public static Integer findMaxNumOfFreeUrinals(String str)
-    {
-        int count;
-        if(goodString(str)) {
-            count = maxNumOfFreeUrinals(str);
-            System.out.println("Number of free urinals :" + count);
+    public static BufferedReader readFile(String fileName) throws IOException {
+        try {
+            FileReader fr = new FileReader(fileName);
+            return new BufferedReader(fr);
+        } catch (Exception e) {
+            return null;
         }
-        else
-        {
+    }
+
+    public static String getOutputFile() throws Exception {
+        File counterFile = new File("counter.txt");
+        Scanner ctr = new Scanner(counterFile);
+        int counter = Integer.parseInt(ctr.nextLine());
+        String outputFile = "rule.txt";
+        if (counter != 0) {
+            outputFile = "rule" + counter + ".txt";
+        }
+        incrementCounter(counter);
+        System.out.println("Successfully written output to " + outputFile);
+        return outputFile;
+    }
+
+    public static Integer findMaxNumOfFreeUrinals(String str) {
+        int count;
+        if (goodString(str)) {
+            count = maxNumOfFreeUrinals(str);
+        } else {
             count = -1;
         }
         return count;
     }
-    public static void writeIntoFile(String outputFile,Integer count) throws Exception
-    {
-        BufferedWriter outputFileBW = new BufferedWriter(new FileWriter(outputFile,true));
+
+    public static void writeIntoFile(String outputFile, Integer count) throws Exception {
+        BufferedWriter outputFileBW = writeFile(outputFile);
         outputFileBW.write(String.valueOf(count));
         outputFileBW.newLine();
         outputFileBW.close();
     }
 
+    public static BufferedWriter writeFile(String fileName) throws IOException
+    {
+    try
+        {
+            FileWriter fw = new FileWriter(fileName, true);
+            return new BufferedWriter(fw);
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
     public static void incrementCounter(int counter) throws Exception
     {
         FileWriter counterFile = new FileWriter("counter.txt");
--- urinalsTest.java
+++ urinalsTest.java
@@ -1,15 +1,27 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class urinalsTest {
+    @Test
+    void readFile() throws IOException {
+        assertEquals(null, urinals.readFile("src/Swetha.txt"));
+        System.out.println("====== Swetha Ganapathy == TEST ONE EXECUTED =======");
+    }
 
+    @Test
+    void writeFile() throws IOException {
+        assertEquals(null, urinals.writeFile("src/Swetha.txt"));
+        System.out.println("====== Swetha Ganapathy == TEST TWO EXECUTED =======");
+    }
     @org.junit.jupiter.api.Test
     void goodString() {
         assertEquals(false, urinals.goodString("myfile"));
         assertEquals(false, urinals.goodString("111000"));
         assertEquals(true, urinals.goodString("1000010"));
-        System.out.println("====== Swetha Ganapathy == TEST ONE EXECUTED =======");
+        System.out.println("====== Swetha Ganapathy == TEST THREE EXECUTED =======");
     }
 
     @Test
@@ -19,14 +31,12 @@ class urinalsTest {
         assertEquals(3, urinals.maxNumOfFreeUrinals("00000"));
         assertEquals(2, urinals.maxNumOfFreeUrinals("0000"));
         assertEquals(-1, urinals.maxNumOfFreeUrinals("011"));
-        System.out.println("====== Swetha Ganapathy == TEST TWO EXECUTED =======");
+        System.out.println("====== Swetha Ganapathy == TEST FOUR EXECUTED =======");
     }
-
-
     @Test
     void validateChoice() {
         assertEquals(true, urinals.validateChoice("1"));
         assertEquals(false, urinals.validateChoice("3"));
-        System.out.println("====== Swetha Ganapathy == TEST THREE EXECUTED =======");
+        System.out.println("====== Swetha Ganapathy == TEST FIVE EXECUTED =======");
     }
 }
